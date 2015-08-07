0339207fe5849be6bc66db3245819b418be4eaea
Axel Magnuson <axelmagn@gmail.com>
2015-08-07 00:11:35 -0600

    write more thorough tests for first envcfg

--- envcfg.py
+++ envcfg.py
@@ -10,15 +10,38 @@ def envcfg(config, env_prefix="ENV:", env_default_sep='='):
     if isinstance(config, dict):
         return {key: envcfg(val) for key, val in config.items()}
 
+    if isinstance(config, list):
+        return [envcfg(val) for val in config]
+
+    if isinstance(config, tuple):
+        return (envcfg(val) for val in config)
+
     elif isinstance(config, str):
         if config.startswith(env_prefix):
+            # trim off the prefix
             env_key = config[len(env_prefix):]
-            env_key = config.split(env_default_sep, maxsplit=1)
-            if len(config) > 1:
+            # split over the default separator
+            env_key = env_key.split(env_default_sep, maxsplit=1)
+            # if a default is set, store it
+            if len(env_key) > 1:
                 config_default = env_key[1]
             else:
                 config_default = None
-            return os.environ.get(env_key[0], config_defaut)
+            # get the environment value from env_val
+            env_val = os.environ.get(env_key[0], config_default)
+            # check that a value was returned
+            if env_val is None:
+                raise KeyError("Environment variable not set: %s" % env_key[0])
+            # parse numerically if possible
+            try:
+                return int(env_val)
+            except ValueError:
+                pass
+            try:
+                return float(env_val)
+            except ValueError:
+                pass
+            return env_val
         else:
             return config
 
---
+++ test_envcfg.py
@@ -0,0 +1,56 @@
+import unittest
+from envcfg import envcfg
+import os
+import json
+
+class TestEnvCfg(unittest.TestCase):
+    def setUp(self):
+        os.environ["WEB_DOMAIN"] = "localhost:5050"
+        os.environ["WORKER_PORT"] = "2020"
+        with open("test_data/config_a.json", 'r') as config_a:
+            self.config_a = envcfg(json.load(config_a))
+
+    def test_primitive_string(self):
+        self.assertIsInstance(self.config_a["hosts"][0]["name"], str)
+        self.assertIsInstance(self.config_a["hosts"][1]["name"], str)
+        self.assertEqual(self.config_a["hosts"][0]["name"], "web")
+        self.assertEqual(self.config_a["hosts"][1]["name"], "worker")
+
+    def test_primitive_float(self):
+        self.assertIsInstance(self.config_a["hosts"][0]["priority"], float)
+        self.assertIsInstance(self.config_a["hosts"][1]["priority"], float)
+        self.assertEqual(self.config_a["hosts"][0]["priority"], 0.1)
+        self.assertEqual(self.config_a["hosts"][1]["priority"], 0.5)
+
+    def test_primitive_int(self):
+        self.assertIsInstance(self.config_a["n_threads"], int)
+        self.assertEqual(self.config_a["n_threads"], 8)
+
+    def test_primitive_list(self):
+        self.assertIsInstance(self.config_a["hosts"], list)
+        self.assertEqual(len(self.config_a["hosts"]), 2)
+
+    def test_primitive_dict(self):
+        self.assertIsInstance(self.config_a["hosts"][0], dict)
+        self.assertIsInstance(self.config_a["hosts"][1], dict)
+
+    def test_env_string(self):
+        self.assertIsInstance(self.config_a["hosts"][0]["domain"], str)
+        self.assertIsInstance(self.config_a["hosts"][1]["domain"], str)
+        self.assertEqual(self.config_a["hosts"][0]["domain"], "localhost:5050")
+        self.assertEqual(
+                self.config_a["hosts"][1]["domain"], "worker.example.com")
+
+    def test_env_int(self):
+        self.assertIsInstance(self.config_a["hosts"][0]["port"], int)
+        self.assertIsInstance(self.config_a["hosts"][1]["port"], int)
+        self.assertEqual(self.config_a["hosts"][0]["port"], 80)
+        self.assertEqual(self.config_a["hosts"][1]["port"], 2020)
+
+    def test_env_float(self):
+        self.assertIsInstance(self.config_a["hosts"][0]["load_ratio"], float)
+        self.assertIsInstance(self.config_a["hosts"][1]["load_ratio"], float)
+        self.assertEqual(self.config_a["hosts"][0]["load_ratio"], 0.6)
+        self.assertEqual(self.config_a["hosts"][1]["load_ratio"], 0.8)
+
+
